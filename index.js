7ee89c584cca7ed7e80ceb13513eb6a523835624
Dildora <48212@edu.raseko.fi>
2024-11-03 20:28:13 +0200

    Done exercises part4 exercise4.1 step 1

---
+++ index.js
@@ -0,0 +1,34 @@
+const express = require('express')
+const app = express()
+require('dotenv').config()
+const Blog = require('./models/blogs')
+const cors = require('cors')
+
+
+app.use(cors())
+app.use(express.json())
+
+app.get('/api/blogs', (request, response) => {
+  Blog
+    .find({})
+    .then(blogs => {
+      response.json(blogs)
+    })
+})
+
+app.post('/api/blogs', (request, response) => {
+  const blog = new Blog(request.body)
+
+  console.log('Resived POST REQUEST', request.body)
+
+  blog
+    .save()
+    .then(result => {
+      response.status(201).json(result)
+    })
+})
+
+const PORT = 3003
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`)
+})
