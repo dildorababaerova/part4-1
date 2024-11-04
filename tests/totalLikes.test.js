430f8c9194c313e1bd08e40a21d8b0009afb1805
Dildora <48212@edu.raseko.fi>
2024-11-04 15:09:53 +0200

    added test

--- tests/totalLikes.test.js
+++ tests/totalLikes.test.js
@@ -3,6 +3,7 @@ const assert = require('node:assert')
 const listHelper = require('../utils/list_helper').totalLikes
 
 describe('total Likes', () => {
+    test('when list has only one blog, equals the likes of that', () => {
     const listWithOneBlog = [
         {
             _id: '5a422aa71b54a676234d17f8',
@@ -13,8 +14,46 @@ describe('total Likes', () => {
             __v: 0
         }]
         
-    test('when list has only one blog, equals the likes of that', () => {
       const result = listHelper(listWithOneBlog)
       assert.strictEqual(result, 5)
+    });
+
+    test('when list has four blogs, equals the likes of that', () => {
+    const listWithfourBlogs = [
+        {
+            _id: '5a422aa71b54a676234d17f8',
+            title: 'Go To Statement Considered Harmful',
+            author: 'Edsger W. Dijkstra',
+            url: 'https://homepages.cwi.nl/~storm/teaching/reader/Dijkstra68.pdf',
+            likes: 5,
+            __v: 0
+        },
+        {
+            _id: '5a422aa71b54a676234d17f8',
+            title: 'Go To Statement Considered Harmful',
+            author: 'Edsger W. Dijkstra',
+            url: 'https://homepages.cwi.nl/~storm/teaching/reader/Dijkstra68.pdf',
+            likes: 5,
+            __v: 0
+        },
+        {
+            _id: '5a422aa71b54a676234d17f8',
+            title: 'Go To Statement Considered Harmful',
+            author: 'Edsger W. Dijkstra',
+            url: 'https://homepages.cwi.nl/~storm/teaching/reader/Dijkstra68.pdf',
+            likes: 5,
+            __v: 0
+        },
+        {
+            _id: '5a422aa71b54a676234d17f8',
+            title: 'Go To Statement Considered Harmful',
+            author: 'Edsger W. Dijkstra',
+            url: 'https://homepages.cwi.nl/~storm/teaching/reader/Dijkstra68.pdf',
+            likes: 5,
+            __v: 0
+        },]
+        
+      const result = listHelper(listWithfourBlogs)
+      assert.strictEqual(result, 20)
     })
 })
