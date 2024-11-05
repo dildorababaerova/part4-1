236eafa15d2f17eb6461b22373ffcd723537ea15
Dildora <48212@edu.raseko.fi>
2024-11-05 14:00:48 +0200

    part4-4 exercise 4.5 done

--- utils/list_helper.js
+++ utils/list_helper.js
@@ -18,6 +18,48 @@ const maxLikes = (blogs) => {
     return Math.max(...blogs.map(blog => blog.likes))
 }
 
+const favoriteBlog = (blogs) => {
+    const blogs = [
+    {
+        _id: '5a422aa71b54a676234d17f8',
+        title: 'Go To Statement Considered Harmful',
+        author: 'Edsger W. Dijkstra',
+        url: 'https://homepages.cwi.nl/~storm/teaching/reader/Dijkstra68.pdf',
+        likes: 2,
+        __v: 0
+    },
+    {
+        _id: '5a422aa71b54a676234d17f8',
+        title: 'Go To Statement Considered Harmful',
+        author: 'Edsger W. Dijkstra',
+        url: 'https://homepages.cwi.nl/~storm/teaching/reader/Dijkstra68.pdf',
+        likes: 7,
+        __v: 0
+    },
+    {
+        _id: '5a422aa71b54a676234d17f8',
+        title: 'Go To Statement Considered Harmful',
+        author: 'Edsger W. Dijkstra',
+        url: 'https://homepages.cwi.nl/~storm/teaching/reader/Dijkstra68.pdf',
+        likes: 9,
+        __v: 0
+    },
+    {
+        _id: '5a422aa71b54a676234d17f8',
+        title: 'Go To Statement Considered Harmful',
+        author: 'Edsger W. Dijkstra',
+        url: 'https://homepages.cwi.nl/~storm/teaching/reader/Dijkstra68.pdf',
+        likes: 21,
+        __v: 0
+    },]
+    
+
+
+    if (!blogs || blogs.length === 0) {
+        return 0
+    }
+    return blogs.reduce((max, blog) => max.likes > blog.likes ? max : blog)
+}
 
 module.exports = {
     dummy,
