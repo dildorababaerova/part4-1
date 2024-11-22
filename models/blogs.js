8d58f04b22a9939b495df428c96fde7ab605b1dd
Dildora <48212@edu.raseko.fi>
2024-11-22 06:29:28 +0200

    lisätty users

--- models/blogs.js
+++ models/blogs.js
@@ -14,6 +14,8 @@ const blogSchema = new mongoose.Schema({
 })
 
 
+
+
 blogSchema.set('toJSON', {
     transform: (document, returnedObject) => {
       returnedObject.id = returnedObject._id.toString()
