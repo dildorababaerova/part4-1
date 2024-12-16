83d0da21a8525c68b9c75642ecc5a210d8f13f0c
Dildora <48212@edu.raseko.fi>
2024-12-16 14:43:29 +0200

    fixed app.jsx

--- app.js
+++ app.js
@@ -26,11 +26,11 @@ app.use(express.json())
 app.use(middleware.requestLogger)
 
 app.use('/api/login', loginRouter)
-app.use('/api/users', usersRouter)
-app.use('/api/blogs', blogsRouter)
 
 app.use(middleware.tokenExtractor)
 app.use(middleware.userExtractor)
+app.use('/api/users', usersRouter)
+app.use('/api/blogs', blogsRouter)
 app.use(middleware.unknownEndpoint)
 app.use(middleware.errorHandler)
 
--- controllers/login.js
+++ controllers/login.js
@@ -29,7 +29,7 @@ loginRouter.post('/', async (request, response) => {
   const token = jwt.sign(
     userForToken,
     process.env.SECRET,
-    { expiresIn: 60*60 }
+    { expiresIn: 600*60 }
   )
 
   response
